e97321ed95f20fd578dd958367b1662fce0086f5
Christian Oliveira <christianoliveira1908@hotmail.com>
2026-02-14 12:03:48 -0300

    Criado controller para envio de email.

---
+++ Lab.SendEmail.API/Controllers/EmailController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.SendEmail.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost("SendEmail")]
+        public IActionResult SendEmail() {
+            return Ok();
+        }
+
+    }
+}
